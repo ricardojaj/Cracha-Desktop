b1e579b971c155ba3334c1616da0d8adaf5a5d0d
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2021-11-02 17:28:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const linksSocialMedia = {
+  github: 'ricardojaj',
+  youtube: 'ricardojaj',
+  facebook: 'ricardojaj',
+  instagram: 'ricardo.jaj',
+  twitter: 'ricardojaj'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+    //alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  //feth pega o json(ele nao sabe que é um json) e transforma em json
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userFoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
